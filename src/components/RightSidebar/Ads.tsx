6f7129e0b821afc5623ce070b1c152f76472fc58
jun.wan <whx10000@gmail.com>
2024-07-07 10:33:13 +0800

    add ads

---
+++ src/components/RightSidebar/Ads.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'preact/hooks'
+
+declare let adsbygoogle: any;
+
+export default function GoAds() {
+    
+    useEffect(()=> {
+        (adsbygoogle = (window as any).adsbygoogle || []).push({});
+    }, [])
+    
+    return (
+        <ins className="adsbygoogle"
+        data-ad-client="ca-pub-5144792726193676"
+        data-ad-slot="7966160203"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+    );
+}
